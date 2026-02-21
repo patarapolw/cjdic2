5da8846a420f2eef66195595355d5d8731484160
polv <polv@polv.cc>
2026-02-22 01:13:11 +0700

    test that flutter_rust_bridge works

---
+++ flutter_app/rust/src/api/mod.rs
@@ -0,0 +1,2 @@
+pub mod cjdic;
+pub mod simple;
--- src-react/src/App.tsx
+++ src-react/src/App.tsx
@@ -1,10 +1,10 @@
-import './App.css';
+import "./App.css";
 
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
-import { invoke } from '@tauri-apps/api/core';
+import { invoke } from "@tauri-apps/api/core";
 
-import reactLogo from './assets/react.svg';
+import reactLogo from "./assets/react.svg";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
