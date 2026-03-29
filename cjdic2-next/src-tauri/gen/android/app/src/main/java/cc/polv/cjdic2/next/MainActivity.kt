7168acd1c41295cde28eb4817aef4560dd5c4521
polv <polv@polv.cc>
2026-03-29 09:15:54 +0700

    scaffold next tauri for sqlx and first run fix

---
+++ cjdic2-next/src-tauri/gen/android/app/src/main/java/cc/polv/cjdic2/next/MainActivity.kt
@@ -0,0 +1,11 @@
+package cc.polv.cjdic2.next
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
